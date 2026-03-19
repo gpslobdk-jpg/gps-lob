3e7e9b21f2e99b558c48176c048c42d2d37599fd
gpslobdk-jpg <gpslobdk@gmail.com>
2026-03-19 21:47:30 +0100

    chore(capacitor): add Capacitor packages and Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dk.gpslob.app',
+  appName: 'GPS Løb',
+  webDir: '.next'
+};
+
+export default config;
