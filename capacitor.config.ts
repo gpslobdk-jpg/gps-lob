f9f5088abcf79acf90396b1a492c4b06c5126e34
gpslobdk-jpg <gpslobdk@gmail.com>
2026-03-19 21:53:41 +0100

    chore(capacitor): set webDir to public and use server URL wrapper

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'dk.gpslob.app',
   appName: 'GPS Løb',
-  webDir: '.next'
+  webDir: 'public',
+  server: {
+    url: 'https://gpslob.dk',
+    cleartext: true
+  }
 };
 
 export default config;
